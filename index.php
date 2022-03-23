9428c16fb69762264fc97e6c0734189bd66a2e13
smsa5382 <98916736+smsa5382@users.noreply.github.com>
2022-03-23 16:09:53 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,6 @@
 <!DOCTYPE html>
 <html style="font-size: 16px;">
   <head>
-    <!--Start of Tawk.to Script-->
-<script type="text/javascript">
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/6231b5cca34c2456412b5084/1fu92sa9u';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
-</script>
-<!--End of Tawk.to Script-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="utf-8">
     <meta name="keywords" content="​SmartHoloMediaUK">
